f728faec71464f5ad054f86e6f19fb7d6f110ce5
Dean Laganiere <Dean.Laganiere@gmail.com>
2023-10-14 01:18:50 -0700

    Update Utils.py
    Removed optional maxsize parameter, setting it to the default of 128

--- worlds/oot/Utils.py
+++ worlds/oot/Utils.py
@@ -11,7 +11,7 @@ def data_path(*args):
     return os.path.join(os.path.dirname(__file__), 'data', *args)
 
 
-@lru_cache(maxsize=None)
+@lru_cache
 def read_json(file_path):
     json_string = ""
     with io.open(file_path, 'r') as file:
